76a53f02aad2c7dc1cdf01f0f3e63412d42e088e
Jose Amat <jose.alc.luis@gmail.com>
2020-03-27 16:57:28 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const data = require("./data.json");
+
+app.use(express.json());
+
+app.get("/clients",function(req, res){
+  res.json(data);
+});
+
+app.get("/clients/:id",function(req, res){
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) return res.status(204).json();
+
+  res.json(client);
+});
+
+app.post("/clients",function(req, res){
+  const { name, email } = req.body;
+
+  // Salvar
+  res.json({ name, email });
+});
+
+app.put("/clients/:id",function(req, res){
+  const { id } = req.params;
+  const client = data.find(cli => cli.id == id);
+
+  if (!client) return res.status(204).json;
+
+  const { name } = req.body;
+
+  client.name = name;
+
+  res.json(client);
+});
+
+app.delete("/clients/:id",function(req, res){
+  const { id } = req.params;
+  const clientsFlitered = data.filter(client => client.id != id);
+
+  res.json(clientsFlitered);
+});
+
+
+
+app.listen(3000, function(){
+  console.log("Server is running");
+});
